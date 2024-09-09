bec3262f9722e14557fae360a3d552a875f79967
Emil Skogsund <emil.skogsund@gmail.com>
2024-09-09 21:23:33 +0200

    feature/keymaps

--- lua/skogix/core/keymaps.lua
+++ lua/skogix/core/keymaps.lua
@@ -1,1 +1,19 @@
-return {}
+local map = vim.keymap.set
+
+-- ';' till ':' i normal mode
+map("n", ";", ":", { desc = "; -> :" })
+-- rensa 's' i normal för search
+map("n", "s", "<Nop>", { desc = "" })
+
+map("n", "s", "<cmd>echo 'test'<cr>", { desc = "skriver ut test" })
+
+local M = {}
+
+-- template för which-key:
+M.testkeys = function()
+  return {
+    {"<leader>s", "<cmd>echo 'test'<cr>", desc = "skriver ut test" }
+  }
+end
+
+return M
