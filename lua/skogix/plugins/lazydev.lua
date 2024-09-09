fbf401faedc4824c87d22688c6e987aa56380bd2
Emil Skogsund <emil.skogsund@gmail.com>
2024-09-09 22:46:34 +0200

    feature/lazydev
    inför lsp

--- lua/skogix/plugins/lazydev.lua
+++ lua/skogix/plugins/lazydev.lua
@@ -1,3 +1,5 @@
+-- uppdatera language server / luals med workspace
+-- https://github.com/folke/lazydev.nvim
 return {
   {
     "folke/lazydev.nvim",
@@ -11,16 +13,22 @@ return {
       },
     },
   },
-  { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-  { -- optional completion source for require statements and module annotations
-    "hrsh7th/nvim-cmp",
-    opts = function(_, opts)
-      opts.sources = opts.sources or {}
-      table.insert(opts.sources, {
-        name = "lazydev",
-        group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-      })
-    end,
-  },
+  -- TODO: vim.uv binds
+  -- https://luvit.io/
+  -- { "Bilal2453/luvit-meta", lazy = true },
+
+  -- NOTE: CMP
+  -- { -- optional completion source for require statements and module annotations
+  --   "hrsh7th/nvim-cmp",
+  --   opts = function(_, opts)
+  --     opts.sources = opts.sources or {}
+  --     table.insert(opts.sources, {
+  --       name = "lazydev",
+  --       group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+  --     })
+  --   end,
+  -- },
+
+  -- NOTE: för framtiden så neodev inte kan användas
   { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
 }
