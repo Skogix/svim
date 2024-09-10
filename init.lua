305ca1ff368e6f85d2eb44bc065ae72c9103720b
Emil Skogsund <emil.skogsund@gmail.com>
2024-09-10 08:29:14 +0200

    refactor: remove unused ReloadBuffer function from init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,3 @@
-function ReloadBuffer()
-  local temp_sync_value = vim.g.aider_buffer_sync                            
-  vim.g.aider_buffer_sync = 0                                                
-  vim.api.nvim_exec2('e!', {output = false})
-  vim.g.aider_buffer_sync = temp_sync_value
-end
 require("skogix.core.options")
 require("skogix.core.lazy")
 require("skogix.core.keymaps")
