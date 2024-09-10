d9851309be0f45eff69730c4d128677cd7337a55
Emil Skogsund <emil.skogsund@gmail.com>
2024-09-11 01:39:27 +0200

    chore: comment out hello function loading in init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,11 @@
 -- This is the main initialization file for the Neovim configuration.
 -- It loads the core modules that set up options, lazy loading, keymaps, and autocommands.
--- It loads the core modules that set up options, lazy loading, keymaps, and autocommands.
 
 require("skogix.core.options")  -- Load and set Neovim options
 require("skogix.core.lazy")     -- Configure lazy loading of plugins
 require("skogix.core.keymaps")  -- Define custom key mappings
 require("skogix.core.autocmds") -- Set up autocommands for various events
-require("skogix.plugins.hello") -- Load the hello function
+-- require("skogix.plugins.hello") -- Load the hello function
+
+
+
