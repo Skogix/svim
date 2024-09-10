02cb85fe589f963716c7f18aa4e99672e49e6803
Emil Skogsund <emil.skogsund@gmail.com>
2024-09-10 08:16:09 +0200

    feature/aider

--- lua/skogix/plugins/aider.lua
+++ lua/skogix/plugins/aider.lua
@@ -9,12 +9,8 @@ return {
   },
   keys = {
     -- Keybinding to open Aider
-    { "<leader>aider", "<cmd>lua AiderOpen()<cr>", desc = "[aider] open aider" },
+    { "<leader>aider", "<cmd>lua AiderOpen(dark-mode)<cr>", desc = "[aider] open aider" },
     -- Keybinding to open Aider in the background
-    { "<leader>abg", "<cmd>lua AiderBackground()<cr>", desc = "[aider] open aider in background" },
-
-    -- Uncomment the lines below to use alternative keybindings
-    -- vim.api.nvim_set_keymap('n', '<leader>oa', '<cmd>lua AiderOpen()<cr>', {noremap = true, silent = true}),
-    -- vim.api.nvim_set_keymap('n', '<leader>ob', '<cmd>lua AiderBackground()<cr>', {noremap = true, silent = true})
+    { "<leader>abg", "<cmd>lua AiderBackground()<cr>", desc = "[aider] open aider in background" }
   },
 }
